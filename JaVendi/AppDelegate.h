ea335d013fc5dd0bf7926710d608c545f8e93dca
robson <robsonsky@gmail.com>
2014-09-02 15:13:25 -0300

    Initial Commit

---
+++ JaVendi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JaVendi
+//
+//  Created by Robson Ribeiro on 9/2/14.
+//  Copyright (c) 2014 rob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JaVendi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JaVendi
+//
+//  Created by Robson Ribeiro on 9/2/14.
+//  Copyright (c) 2014 rob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
